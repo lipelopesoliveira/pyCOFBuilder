2c7f82795917958948288896f02f7304551a70ff
Felipe Lopes de Oliveira <33868364+lipelopesoliveira@users.noreply.github.com>
2021-06-10 20:13:35 -0300

    Update

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@
 
 from setuptools import setup
 
-
 with open('LICENSE') as f:
     license = f.read()
 
@@ -20,7 +19,7 @@ setup(
     url='https://github.com/lipelopesoliveira/pyCOFBuilder',
     license=license,
     package_dir={'':'pycofbuilder'},
-    install_requires=['os', 'numpy', 'scipy', 'pymatgen>=2022.0.8'], 
+    install_requires=['os', 'math', 'numpy>=1.2', 'scipy>=1.6.3', 'pymatgen>=2022.0.8'], 
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
