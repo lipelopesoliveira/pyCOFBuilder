4064817e64aeed227b69d3dc88a82da50255a69a
Felipe Lopes de Oliveira <33868364+lipelopesoliveira@users.noreply.github.com>
2021-09-07 09:55:04 -0300

    Update tools.py

--- pycofbuilder/tools.py
+++ pycofbuilder/tools.py
@@ -730,14 +730,14 @@ def save_pqr(file_path, file_name, cell, atom_label, atom_pos, partial_charges=F
     pqr_file = open(os.path.join(file_path, file_name + '.pqr'), 'w')
     pqr_file.write(f'TITLE       {file_name}  \n')
     pqr_file.write('REMARK   4\n')
-    pdb_file.write(f'CRYST1{cell[0]:>9.3f}{cell[1]:>9.3f}{cell[2]:>9.3f}{cell[3]:>7.2f}{cell[4]:>7.2f}{cell[5]:>7.2f} P1\n')
+    pqr_file.write(f'CRYST1{cell[0]:>9.3f}{cell[1]:>9.3f}{cell[2]:>9.3f}{cell[3]:>7.2f}{cell[4]:>7.2f}{cell[5]:>7.2f} P1\n')
 
     if partial_charges is not False:
         for i in range(len(atom_pos)):
-            pqr_file.write(f'ATOM   {i+1:>4} {atom_label[i]:>2}   MOL A   0      {atom_pos[i][0]:>8.3f}{atom_pos[i][1]:>8.3f}{atom_pos[i][2]:>8.3f}  {partial_charges:>3.5f}                {atom_label[i]}\n')
+            pqr_file.write(f'ATOM   {i+1:>4} {atom_label[i]:>2}   MOL A   0    {atom_pos[i][0]:>8.3f}{atom_pos[i][1]:>8.3f}{atom_pos[i][2]:>8.3f}{partial_charges:>8.5f}                {atom_label[i]}\n')
     if partial_charges is False:
         for i in range(len(atom_pos)):
-            pqr_file.write(f'ATOM   {i+1:>4} {atom_label[i]:>2}   MOL A   0      {atom_pos[i][0]:>8.3f}{atom_pos[i][1]:>8.3f}{atom_pos[i][2]:>8.3f}                {atom_label[i]}\n')
+            pqr_file.write(f'ATOM   {i+1:>4} {atom_label[i]:>2}   MOL A   0    {atom_pos[i][0]:>8.3f}{atom_pos[i][1]:>8.3f}{atom_pos[i][2]:>8.3f}                {atom_label[i]}\n')
 
     pqr_file.close()
     
