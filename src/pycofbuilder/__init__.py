a2e9ce8e2953040b71c0074d4f6d7f6833d1c669
Felipe Lopes <lipe.lopes.oliveira@gmail.com>
2023-11-13 19:54:41 -0300

    Update init

--- src/pycofbuilder/__init__.py
+++ src/pycofbuilder/__init__.py
@@ -20,11 +20,13 @@ from pycofbuilder.framework import Framework
 import pycofbuilder.tools as Tools
 import pycofbuilder.io_tools as IO_Tools
 
-__all__ = ['BuildingBlock',
-           'Framework',
-           'Tools',
-           'IO_Tools'
-           ]
+__all__ = [
+    'BuildingBlock',
+    'Framework',
+    'Tools',
+    'IO_Tools',
+    'ChemJSON'
+    ]
 
 _ROOT = os.path.abspath(os.path.dirname(__file__))
 
