009fb33d490d1e48ee2dfe0c531889f6ebac15d9
Felipe Lopes <lipe.lopes.oliveira@gmail.com>
2024-06-18 14:33:48 -0300

    Bump bersion to 0.0.8.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('LICENSE') as f:
     license = f.read()
 
 
-VERSION = '0.0.8.4'
+VERSION = '0.0.8.5'
 DESCRIPTION = 'A package for Covalent Organic Frameworks sturcture assembly.'
 
 setup(
