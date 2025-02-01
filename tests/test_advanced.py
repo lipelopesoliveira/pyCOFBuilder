ecb3ecafac7438823705777eb051af47bed4e416
Felipe Lopes <lipe.lopes.oliveira@gmail.com>
2025-02-01 18:45:58 +0100

    Update requirements and enhance basic tests: add pytest and pytest-cov, refactor test cases for clarity and structure

--- tests/test_advanced.py
+++ tests/test_advanced.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 
-from .context import sample
+#from .context import sample
 
 import unittest
 
 
-class AdvancedTestSuite(unittest.TestCase):
-    """Advanced test cases."""
+#class AdvancedTestSuite(unittest.TestCase):
+#    """Advanced test cases."""
 
-    def test_thoughts(self):
-        self.assertIsNone(sample.hmm())
+ #   def test_thoughts(self):
+#        self.assertIsNone(sample.hmm())
 
 
-if __name__ == '__main__':
-    unittest.main()
+#if __name__ == '__main__':
+ #   unittest.main()
