8dfc38752c646066960c8bd5e58cad73dcc52b26
Felipe Lopes <lipe.lopes.oliveira@gmail.com>
2025-05-01 15:39:40 +0200

    Bump version to 0.0.9.3 in CITATION.cff, pyproject.toml, setup.py, and __init__.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('LICENSE') as f:
     license_file = f.read()
 
 
-VERSION = '0.0.9.2'
+VERSION = '0.0.9.3'
 DESCRIPTION = 'A package for Covalent Organic Frameworks sturcture assembly.'
 
 setup(
